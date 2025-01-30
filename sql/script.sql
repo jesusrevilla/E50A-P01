81accb133f1268ff1e38e16a5987f4a623f4435c
Alfredo De Alba Sánchez <dealbaa347@gmail.com>
2025-01-29 18:46:28 -0600

    177847_idx.sql
    creation of the index in name

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,17 @@
 
+-- create
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL UNIQUE PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL
+);
+
+--create index
+CREATE INDEX idx_name ON users(name);
+
+-- insert(create)
+INSERT INTO users  (name,email)VALUES ('Alice', 'alice@example.com');
+INSERT INTO users  (name,email)VALUES ('Bob', 'bob@example.com');
+
+-- fetch (read)
+SELECT name FROM users;
