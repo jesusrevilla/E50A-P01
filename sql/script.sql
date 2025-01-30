997cba2ab7b818467d1d502df2f0e70a97a6dfa2
ale <88249659+sopadepollo@users.noreply.github.com>
2025-01-29 18:21:55 -0600

    Update script.sql 177890

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,15 @@
 
+-- create
+CREATE TABLE USERS (
+  id SERIAL PRIMARY KEY,
+  name varchar(100),
+  email varchar(100) 
+);
+
+-- insert
+INSERT INTO USERS (name, email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO USERS (name, email) VALUES ('Bob', 'bob@example.com');
+
+-- fetch 
+SELECT * FROM USERS;
+
