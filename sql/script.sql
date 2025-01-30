58d9f8722eda828b152d825021b6f9647aecf6db
170973 <170973@upslp.edu.mx>
2025-01-29 18:28:09 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,14 @@
+-- create
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100)
+);
+
+-- insert
+INSERT INTO users (name, email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO users (name, email) VALUES ('Bob', 'bob@example.com');
+
+-- fetch 
+ SELECT * FROM users;
 
