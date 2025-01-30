d133beda44f02aa8dee0b95d50f8981a2374b031
Alfredo De Alba Sánchez <dealbaa347@gmail.com>
2025-01-29 18:36:24 -0600

    177847.sql
    correction in bob

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,18 @@
 
+-- create
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL UNIQUE PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL
+);
+
+
+-- insert(create)
+INSERT INTO users  (name,email)VALUES ('Alice', 'alice@example.com');
+INSERT INTO users  (name,email)VALUES ('Bob', 'bob@example.com');
+
+-- fetch (read)
+SELECT * FROM users;
+
+
+
