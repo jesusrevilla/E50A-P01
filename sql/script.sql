f56b907e0e0eb22ba0396fa5ad38279e3ad774fb
180519 <161090823+180519@users.noreply.github.com>
2025-01-29 18:20:14 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,19 @@
+-- Universidad Politécnica de   San Luis Potosi
+-- Base de Datos
+-- 29 de Enero, 2025
+-- Christian Alejandro Cárdenas Rucoba
 
+
+-- create
+CREATE TABLE USERS (
+  id SERIAL PRIMARY KEY,
+  name varchar NOT NULL,
+  email varchar NOT NULL
+);
+
+-- insert
+INSERT INTO USERS (name,email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO USERS (name,email) VALUES ('Bob', 'bob@example.com');
+
+-- fetch 
+SELECT * FROM USERS;
