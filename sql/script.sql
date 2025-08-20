dfe1cf01c4b537af42adaafe71a9371ba3044c6a
Fano-Rdz <178584@upslp.edu.mx>
2025-08-19 19:29:43 -0600

    Update 1

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,11 @@
+CREATE TABLE users(
+  id-users SERIAL PRIMARY KEY,
+  name VARCHAR(15) NOT NULL,
+  email VARCHAR(30) NOT NULL
+);
 
+INSERT INTO users (nombre, email) VALUES
+('Alice', 'Alice@hotmail.com'),
+('Bob', 'Bob@hotmail.com');
+
+SELECT * FROM users;
