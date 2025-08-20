0d22676eadd16bce129b60e56afb3933d68374e6
Karina123Kma <upslpbd@gmail.com>
2025-08-19 19:46:32 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,10 +1,10 @@
--- users
+
 CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100) UNIQUE    
+id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(100) UNIQUE    
     );
--- insert users
+
 INSERT INTO users (name, email) VALUES
 ('Alice', 'alice@example.com'),
 ('Bob', 'bob@example.com');
