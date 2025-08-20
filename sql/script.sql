aa86fbc912a838724561e5ecbef3570538da9256
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-08-19 19:30:13 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,14 @@
+-- Tabla ESTUDIANTE
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  email VARCHAR(30) NOT NULL
+);
 
+-- Insertar datos (con IDs manuales)
+INSERT INTO users (id, name, email) VALUES
+(1, 'Alice', '179827@upslp.edu.mx'),
+(2, 'Bob', '196354@upslp.edu.mx');
+
+-- Mostrar los datos insertados
+SELECT * FROM users;
