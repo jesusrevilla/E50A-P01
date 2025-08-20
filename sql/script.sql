8e5457a9ce65765f62e619cb2f465ed07db30ecf
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-08-19 19:27:32 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,11 @@
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  email VARCHAR(30) NOT NULL
+);
 
+INSERT INTO users (name,email) VALUES 
+  ('Alice','Alice@gmail.com'),
+  ('Bob','Bob@gmail.com');
+
+  
