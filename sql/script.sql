d63ce14f413e9a3161aaec39f89ab4ba201304b0
aldogg10 <179862@upslp.edu.mx>
2025-08-19 19:41:21 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -3,3 +3,6 @@ id_usuario SERIAL PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL
 );
+
+INSERT INTO users (name, email) VALUES ('Alice', 'alice@correo.com');
+INSERT INTO users (name, email) VALUES ('Bob', 'bob@correo.com');
