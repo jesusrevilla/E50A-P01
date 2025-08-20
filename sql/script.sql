4bbec354ace1f4cc1e2ac4456dc8d8e585c8f532
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-08-20 14:21:33 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,12 @@
+--Yazmin Guerrero Guevara - 182483
+--Crear tabla users
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30)  NOT NULL,
+    email VARCHAR (30)  NOT NULL
+  );
+--insertar registros
+INSERT INTO users (name, email) VALUES
+('Alice', 'alice@outlook.com'),
+('Bob', 'bob@outlook.com');
 
