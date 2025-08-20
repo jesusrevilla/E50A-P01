88102aa8841fac7f41936ea580781b2c17360cb4
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-08-19 19:26:37 -0600

    feature/182239

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,10 @@
+-- create
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
 
+-- insert
+INSERT INTO users VALUES (0001, 'Alice', 'alice@gmail.com');
+INSERT INTO users VALUES (0001, 'Bob', 'bob@gmail.com');
