88c741a196ac27ae06aaee2dc62f279f5963d150
175329 <175329@upslp.edu.mx>
2025-08-19 19:30:34 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,10 @@
+CREATE TABLE users(
+  usr_id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
 
+INSERT INTO users (name) VALUES ('Alice');
+INSERT INTO users (name) VALUES ('Bob');
+
+SELECT * FROM users;
