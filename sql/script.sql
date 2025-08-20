1cdb5860a7c3ed4cf4bc308e0c4ca7844d74f357
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-08-19 19:32:49 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,10 +1,8 @@
--- create
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL
 );
 
--- insert
 INSERT INTO users VALUES (0001, 'Alice', 'alice@gmail.com');
 INSERT INTO users VALUES (0001, 'Bob', 'bob@gmail.com');
