5614a44ad5bdd365d21199c7939a2d1347523a36
Jpablo179804 <179804@upslp.edu.mx>
2025-08-19 19:33:58 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,8 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL
+);
 
+INSERT INTO users (name, email) values ('Alice', 'alice@gmail.com');
+INSERT INTO users VALUES (2, 'Bob', 'bob@gmail.com');
