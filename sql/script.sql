2eb4dfd9cbf4290f8dc08744cddd7babc9a7f7eb
Karina123Kma <upslpbd@gmail.com>
2025-08-19 19:38:40 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,11 @@
+CREATE TABLE users (
+ id tipo SERIAL PRIMARY KEY,
+ name tipo VARCHAR NOT NULL,
+ email tipo VARCHAR NOT NULL
+  );
 
+INSERT INTO users (id, name, email) VALUES
+(1, 'Alice', '178390@upslp.edu.mx'),
+(2, 'Bob', '153890@upslp.edu.mx');
+
+SELECT * FROM users;
