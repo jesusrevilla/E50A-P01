8ac37c86e43a70686431fa28110a1beb53a7f808
Diego0086 <178218@upslp.edu.mx>
2025-08-20 19:14:12 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,10 @@
+-- create
+CREATE TABLE USERS (
+  columna INTEGER PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL
+);
 
+-- insert
+INSERT INTO USERS VALUES (1, 'Alice', 'Alice@gmail.com');
+INSERT INTO USERS VALUES (2, 'Bob', 'Bob@gmail.com');
