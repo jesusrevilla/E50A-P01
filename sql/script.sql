8e13b5f1733cfd526fc772ef425dfb20bc979572
rafaelSanchez4330 <rafaelsanchez9924@gmail.com>
2025-08-20 19:08:12 -0600

    feature/182712

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,8 @@
+CREATE TABLE users(
+  id_users SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
 
+INSERT INTO users VALUES('Alice', 'alice123@gmail.com');
+INSERT INTO users VALUES('Bob', 'Bob123@gmail.com');
