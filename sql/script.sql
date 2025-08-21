bd0c6e9fa24bbfb7498bc036c0490ff7f77a0818
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-08-20 19:19:30 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,11 @@
 
+-- Crear la tabla users
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+);
+
+-- Insertar dos registros
+INSERT INTO users (name, email) VALUES ('Alice', 'alice@upslp.com');
+INSERT INTO users (name, email) VALUES ('Bob', 'bob@upslp.com');
