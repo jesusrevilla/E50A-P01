f7aeb4a3fbafd2dc58b1f56335f0ddfa417d50d7
andreupslpornelas <182085@upslp.edu.mx>
2025-08-20 19:15:37 -0600

    Update script.sql
    New sql script

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
 
+
+INSERT INTO users (name, email) VALUES ('Alice', 'alice@example.com');
+
+INSERT INTO users (name, email) VALUES ('Bob', 'bob@example.com');
